b49b1f42035ecd851437af4eb11b0dd2a98e64c8
haoxiang li <li001294@umn.edu>
2023-05-09 20:18:42 -0500

    monotone function update

--- R/ismonotone.R
+++ R/ismonotone.R
@@ -1,17 +1,20 @@
 # monotone check
-ismonotone <- function(yobs)
+ismonotone <- function(yobs, X)
 {
   # Check whether yobs is monotone or not, and permute yobs into the standard monotone format after permuting the rows and columns.
   # Args:
-  #   yobs: matrix. Monotone Matrix 
+  #   yobs: matrix. Monotone matrix.
+  #   X: matrix. Feature matrix.
   # Returns:
   #   List. monotone: logic. TRUE if yobs is monotone; FALSE otherwise.
-  #         ynew: matrix. yobs in standard monotone format/ clothest to monotone.
+  #         ynew: matrix. yobs in standard monotone format/ closest to monotone.
   #         ynew_column: vector. ynew (permuted) original column location in yobs.
   #         ynew_row: vector. ynew (permuted) original row location in yobs.
-
+  #         Xnew: matrix. the corresponding feature matrix after rearranging the rows. 
+  #         permute: logic. TRUE if permutation is needed (to be monotone/ closest to monotone); FALSE if no need to permute.
 
   yobs <- as.matrix(yobs) 
+  X <- as.matrix(X)
   I <- ! is.na(yobs) # missing index : observed TRUE  missing FALSE
   row_obs <- rowSums(I, na.rm = T) 
   row_sort <- order(row_obs, decreasing = TRUE) 
@@ -19,8 +22,10 @@ ismonotone <- function(yobs)
   col_sort <- order(col_obs, decreasing = FALSE) 
   I_new <- I[row_sort, col_sort, drop = FALSE]
   ynew <- yobs[row_sort, col_sort, drop = FALSE]
+  Xnew <- X[row_sort, , drop = FALSE]
   ni <- colSums(I_new, na.rm = T)
   monotone <- TRUE
+  permute = !all(row_sort == c(1:length(row_sort))) * all(col_sort == c(1:length(col_sort))) 
   if (ni[1] == 0){
     stop("An observation should contain at least one observed entry")
   }
@@ -38,6 +43,6 @@ ismonotone <- function(yobs)
   }
   names(col_sort) = names(yobs)
   ynew = as.matrix(ynew)
-  return(list(monotone = monotone, ynew = ynew, ynew_column = col_sort, ynew_row = row_sort))
+  return(list(monotone = monotone, ynew = ynew, ynew_column = col_sort, ynew_row = row_sort, Xnew = Xnew, permute = permute))
 }
 
